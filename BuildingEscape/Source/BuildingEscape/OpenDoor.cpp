f7a931329e65ed6de30fdfce609ffad65328cb22
DESKTOP-68NOR39\Happy <dipankarjoy15@gmail.com>
2017-12-19 19:35:17 +0530

    BE11 Moving Objects with Cpp

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Find the Owning Actor
+	AActor* Owner = GetOwner();
+
+	//create a rotator
+	FRotator NewRotation = FRotator(0.f, -60.f, 0.f); //or not necessarily needa add f
+
+	/* MY APPROACH
+	FRotator NewRotation;
+	NewRotation.Pitch = 0.f;
+	NewRotation.Yaw = -60.f;
+	NewRotation.Roll = 0.f;
+	*/
+
+
+	//set the door rotation
+	Owner->SetActorRotation(NewRotation);
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
