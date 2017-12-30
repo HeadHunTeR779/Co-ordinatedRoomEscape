2ed203c7b3fa9c16ba23df29293f3d9404986bf0
DESKTOP-68NOR39\Happy <dipankarjoy15@gmail.com>
2017-12-30 19:26:16 +0530

    BE24 Line Trace Single By Object Type

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -33,22 +33,40 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
 	
-	//Get Player's viewpoint this tick
+	///Get Player's viewpoint this tick
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
 
+	
 	//TODO log out to test
 	/*UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());*/
 
-	//draw a red trace in the world to visualise
+	
+	///draw a red trace in the world to visualise
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
-
 	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.f, 0, 10.f);
 
+	///Setup Qery parameters (For raycast below)
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
 
-	//Ray-cast out to reach distance (like a one-eyed superman using laser eyes)
+	///Ray-cast (line trace) out to reach distance (like a one-eyed superman using laser eyes)
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation, 
+		LineTraceEnd, 
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), 
+		TraceParameters
+	);
 
-	//See what we hit
+
+	///See what we hit
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit)								//if no actor hit then we simply skip this
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Line Trace Hit: %s"), *(ActorHit->GetName()));
+	}
+	
 }
 
